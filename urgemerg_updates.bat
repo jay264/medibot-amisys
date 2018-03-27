b3841ccf79f9d8bee62325feb1944aa637b6be4f
Brent Lusenhop <brent.lusenhop@mchs.com>
2018-03-27 05:34:44 -0400

    Update batch files for jruby

--- urgemerg_updates.bat
+++ urgemerg_updates.bat
@@ -1,2 +1,2 @@
-call cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-03-23_thru_2018-03-25_urgemerg.feature
-call cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-03-26_urgemerg.feature
+call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-03-23_thru_2018-03-25_urgemerg.feature
+call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-03-26_urgemerg.feature
