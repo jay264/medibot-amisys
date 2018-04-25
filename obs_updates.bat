f2d6b5015385659d36fcd333c293174927e828be
Jeff Lusenhop <jeffrey.lusenhop@mchs.com>
2018-04-25 06:58:55 -0400

    Production 20180424

--- obs_updates.bat
+++ obs_updates.bat
@@ -1,2 +1,3 @@
-call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-04-18_obs.feature
-call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-04-19_obs.feature
+call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-04-20_thru_2018-04-22_obs.feature
+call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-04-23_obs.feature
+call jruby -S cucumber URL=PROD features/processed_EssetteDailyExtractReport-Inpatient(NEWADD)-2018-04-24_obs.feature
